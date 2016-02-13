7095eddff0617a0557c7aa7d87a2b1cae3bc3ad3
atong01 <alexanderytong@gmail.com>
2016-02-13 00:46:40 -0500

    changes to run script allowing multiple image files to be specified and run

--- README.md
+++ README.md
@@ -8,7 +8,18 @@ docker run atong01/imagenet-tensorflow python classify_image.py
 ```
 or
 ```
-sh run.sh
+sh run.sh 
+```
+
+expected output
+```
+running on default
+-----------------------------------------------------------------------------
+giant panda, panda, panda bear, coon bear, Ailuropoda melanoleuca (score = 0.89233)
+indri, indris, Indri indri, Indri brevicaudatus (score = 0.00859)
+lesser panda, red panda, panda, bear cat, cat bear, Ailurus fulgens (score = 0.00264)
+custard apple (score = 0.00141)
+earthstar (score = 0.00107)
 ```
 
 Or, to use your own image file:
--- run.sh
+++ run.sh
@@ -1,1 +1,16 @@
-docker run -v $PWD:/root/tmp:ro atong01/imagenet-tensorflow python classify_image.py --image_file tmp/$1 2> /dev/null
+#!/bin/bash
+if [ "$#" -eq 0 ];
+then
+    echo running on default
+    echo -----------------------------------------------------------------------------
+    IMAGE=''
+    docker run -v $PWD:/root/tmp:ro atong01/imagenet-tensorflow python classify_image.py $IMAGE 2> /dev/null
+fi
+for var in "$@"
+do
+    IMAGE='--image_file tmp/'$var
+    echo running on $var
+    echo -----------------------------------------------------------------------------
+    docker run -v $PWD:/root/tmp:ro atong01/imagenet-tensorflow python classify_image.py $IMAGE 2> /dev/null
+    echo
+done
